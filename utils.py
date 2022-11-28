c3a022b9d429e37628eb02076cc638932cd54c80
Platon <97834402+Master-Stroke@users.noreply.github.com>
2022-11-28 04:05:11 +0200

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,26 @@
+import typing
+
+
+def get_restriction_time(string: str) -> typing.Optional[int]:
+    """
+    Get user restriction time in seconds
+    :string: string to check for multiplier. The last symbol should be one of:
+        "m" for minutes, "h" for hours and "d" for days
+    :return: number of seconds to restrict or None if error
+    """
+    if len(string) < 2:
+        return None
+    letter = string[-1]
+    try:
+        number = int("".join(c for c in string if c.isdecimal()))
+    except TypeError:
+        return None
+    else:
+        if letter == "m":
+            return 60 * number
+        elif letter == "h":
+            return 3600 * number
+        elif letter == "d":
+            return 86400 * number
+        else:
+            return None
